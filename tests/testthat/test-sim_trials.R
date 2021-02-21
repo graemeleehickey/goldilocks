340209335eaff327f0264ea1b75ebfdf93bb8f18
graemeleehickey <graemeleehickey@gmail.com>
2021-02-21 21:47:18 +0000

    New key test

---
+++ tests/testthat/test-sim_trials.R
@@ -0,0 +1,30 @@
+test_that("sim_trials", {
+  hc <- prop_to_haz(c(0.20, 0.30), 12, 36)
+  ht <- prop_to_haz(c(0.05, 0.15), 12, 36)
+
+  out <- sim_trials(
+    hazard_treatment = ht,
+    hazard_control = hc,
+    cutpoint = c(0, 12),
+    N_total = 600,
+    lambda = 20,
+    lambda_time = NULL,
+    interim_look = c(400, 500),
+    end_of_study = 36,
+    prior = c(0.1, 0.1),
+    block = 2,
+    rand_ratio = c(1, 1),
+    prop_loss_to_followup = 0.30,
+    alternative = "two.sided",
+    h0 = 0,
+    futility_prob = 0.05,
+    expected_success_prob = 0.9,
+    prob_ha = 0.975,
+    N_impute = 2,
+    N_mcmc = 2,
+    N_trials = 2,
+    method = "logrank",
+    ncores = 1)
+
+  expect_s3_class(out, "data.frame")
+})
